001f44ce404c2c85bc2780fd6b389d9a92b58602
wildmolasses <changes@gmail.com>
2022-02-02 08:14:41 -0800

    oracle deploy script

---
+++ contracts/scripts/deploy/deployUniV3ReimbursementOracle.ts
@@ -0,0 +1,22 @@
+import { ethers } from "hardhat";
+import { UniV3ReimbursementOracle__factory } from "../../typechain/factories/UniV3ReimbursementOracle__factory";
+type DeployRecord = {
+  address: string;
+  constructorArgs?: unknown[];
+};
+
+export const deployUniV3ReimbursementOracle = async (
+  context: Record<string, DeployRecord>,
+  ...args: Parameters<UniV3ReimbursementOracle__factory["deploy"]>
+) => {
+  const contract = await ethers
+    .getContractFactory("UniV3ReimbursementOracle")
+    .then((factory: UniV3ReimbursementOracle__factory) => factory.deploy(...args));
+  await contract.deployed();
+  context.UniV3ReimbursementOracle = {
+    address: contract.address,
+    constructorArgs: args,
+  };
+  console.log(`UniV3ReimbursementOracle deployed to: ${contract.address}`);
+  return contract;
+};
