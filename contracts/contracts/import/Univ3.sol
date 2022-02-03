aedcbfbae8322cb00d81700d8972970a114c9a69
wildmolasses <changes@gmail.com>
2022-02-03 12:11:55 -0800

    make import file an interface

--- contracts/contracts/import/Univ3.sol
+++ contracts/contracts/import/Univ3.sol
@@ -6,7 +6,7 @@ import "@uniswap/v3-periphery/contracts/NonfungiblePositionManager.sol";
 import "@uniswap/v3-periphery/contracts/SwapRouter.sol";
 import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
 
-contract Uni {
+interface Uni {
   // This file is a dev convenience file that helps us use contract types in tests.
   // It will never be deployed.
 }
