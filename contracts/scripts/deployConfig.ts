de05f2313e2000c72d87e2bfc6678f7331e30b27
wildmolasses <changes@gmail.com>
2022-02-10 09:57:16 -0800

    Collateral token and oracle are optional

--- contracts/scripts/deployConfig.ts
+++ contracts/scripts/deployConfig.ts
@@ -10,7 +10,8 @@ export const config: Record<string, deployConfig> = {
       // Here, supply is 1,000,000 riTokens
       supply: units.wad(1000000),
       // Token that underlies the reimbursement token
-      treasuryToken: "0xaD6D458402F60fD3Bd25163575031ACDce07538D", // DAI
+      // Here, Ropsten DAI
+      treasuryToken: "0xaD6D458402F60fD3Bd25163575031ACDce07538D",
       // How long to wait before maturation / reimbursement calc finalization
       // Here, 1 hour in the future
       maturity: Math.floor(Date.now() / 1000) + 60 * 60,
@@ -20,14 +21,17 @@ export const config: Record<string, deployConfig> = {
     riPool: {
       // targeting 1 reimbursement token = 3 underlying treasury tokens
       targetExchangeRate: units.wad(3),
-      // optional collateral token
+      // Optional collateral token to collect and, if shortfall on maturity, distribute
+      // Here, Ropsten WETH
       collateralToken: "0xc778417E063141139Fce010982780140Aa0cD5Ab", // WETH
     },
+    // Oracle config: only necessary if collateralToken set in riPool
     oracle: {
-      // DAI / WETH pool at fee 0.3%
+      // Ropsten DAI / WETH pool with fee 0.3%
       // see getPool @ https://ropsten.etherscan.io/address/0x1F98431c8aD98523631AE4a59f267346ea31F984#readContract
       uniV3Pool: "0x40FDe2952a0674a3E77707Af270af09800657293",
       // Specifies the interval in seconds that the oracle's TWAP should calculate the mean over
+      // Used to value collateral token in terms of treasury token if collateral token needs to be distributed
       // here, 600 seconds or 10 min
       twapPeriod: 600,
     },
@@ -43,7 +47,7 @@ export type deployConfig = {
     maturity: number;
   };
   merkleRoot?: string;
-  oracle: {
+  oracle?: {
     uniV3Pool: string;
     twapPeriod: number;
   };
