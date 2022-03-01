8bb511ca8812746343333e7c60388360e7ed0e7a
wildmolasses <changes@gmail.com>
2022-02-28 23:44:22 -0600

    add merkle distributor to dev deploy

--- contracts/scripts/deployDevEnvironment.ts
+++ contracts/scripts/deployDevEnvironment.ts
@@ -3,6 +3,7 @@ import { deployMockToken } from "./deploy/deployMockToken";
 import { deployMockOracle } from "./deploy/deployMockOracle";
 import { deployReimbursementPool } from "./deploy/deployReimbursementPool";
 import { deployReimbursementToken } from "./deploy/deployReimbursementToken";
+import { deployMerkleDistributor } from "./deploy/deployMerkleDistributor";
 import { record, DeployDetail } from "./deploy/helpers";
 
 async function main() {
@@ -11,15 +12,21 @@ async function main() {
     const signers = await ethers.getSigners();
     const usdc = await deployMockToken(deployContext, "USDC", "USDC", 6);
     await usdc.mint(signers[0].address, 1000000000000);
+    const supply = ethers.utils.parseUnits("1000000", 18);
     const riToken = await deployReimbursementToken(
       deployContext,
       "my token",
       "TOK",
       10000000000,
       usdc.address,
-      1000000,
+      supply,
       signers[0].address,
     );
+    // Change to your merkle root
+    const merkleRoot = "0x0000000000000000000000000000000000000000000000000000000000000000";
+    const distributor = await deployMerkleDistributor(deployContext, riToken.address, merkleRoot);
+    const transfer = await riToken.transfer(distributor.address, supply);
+    console.log(`Transferring riToken supply to merkle distributor:  ${transfer.hash}`);
     const oracle = await deployMockOracle(deployContext, 100000);
     await deployReimbursementPool(
       deployContext,
